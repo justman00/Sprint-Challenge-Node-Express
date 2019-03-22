b627cccbf76cb3617a52890418850186aa0a9ab6
Vlad <vovaturcan4@gmail.com>
2019-03-22 17:10:30 +0200

    installed dependencies and added the boilerplate for the server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`<h1>Welcome Home</h1>`);
+});
+
+module.exports = server;
